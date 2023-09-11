78f5d65bb82ba8c0fb7b43bf059688014f6aaeba
Rishabh Chauhan <119417999+a-fai1ur3@users.noreply.github.com>
2023-09-12 03:18:20 +0530

    Update viewAndpure.sol

--- viewAndpure.sol
+++ viewAndpure.sol
@@ -1,40 +1,48 @@
 // SPDX-License-Identifier: MIT
-// The above comment specifies the license under which this contract is distributed.
-
 pragma solidity ^0.8.17;
-// Specifies the version of the Solidity compiler to use. In this case, version 0.8.17 or higher is required.
 
+/**
+ * @title ViewAndPureFunctions
+ * @notice A Solidity smart contract showcasing view and pure functions.
+ */
 contract ViewAndPureFunctions {
-    // Declaring a Solidity smart contract named "ViewAndPureFunctions."
-
+    /**
+     * @dev Public unsigned integer state variable.
+     */
     uint public num;
-    // Declaring a public unsigned integer state variable named "num."
 
+    /**
+     * @dev Function to view the current value of 'num'.
+     * @return The current value of 'num'.
+     */
     function viewFunc() public view returns (uint) {
-        // Declaring a public view function named "viewFunc" with no arguments.
-        // This function is marked as "view," indicating that it only reads the contract's state and doesn't modify it.
-
-        return num; // Return the current value of the "num" state variable.
+        return num;
     }
 
+    /**
+     * @dev Function to demonstrate a pure function.
+     * @return The constant value 1.
+     */
     function pureFunc() public pure returns (uint) {
-        // Declaring a public pure function named "pureFunc" with no arguments.
-        // This function is marked as "pure," meaning it doesn't read or modify the contract's state.
-
-        return 1; // Return the constant value 1.
+        return 1;
     }
     
+    /**
+     * @dev Function to add a value to 'num'.
+     * @param x The value to add to 'num'.
+     * @return The result of adding 'x' to 'num'.
+     */
     function addToNum(uint x) public view returns (uint) {
-        // Declaring a public view function named "addToNum" that takes an unsigned integer argument "x."
-        // This function is marked as "view," as it reads the contract's state.
-
-        return num + x; // Return the result of adding "x" to the current value of the "num" state variable.
+        return num + x;
     }
     
+    /**
+     * @dev Function to add two numbers.
+     * @param x The first number to add.
+     * @param y The second number to add.
+     * @return The result of adding 'x' and 'y'.
+     */
     function add(uint x, uint y) public pure returns (uint) {
-        // Declaring a public pure function named "add" that takes two unsigned integer arguments, "x" and "y."
-        // This function is marked as "pure" and doesn't read or modify the contract's state.
-
-        return x + y; // Return the result of adding "x" and "y."
+        return x + y;
     }
 }
