b696cec3b817237217de51601ca8d04eb7d27433
Rishabh Chauhan <119417999+a-fai1ur3@users.noreply.github.com>
2023-09-12 02:45:47 +0530

    Update valueTypes.sol

--- valueTypes.sol
+++ valueTypes.sol
@@ -4,9 +4,9 @@
 pragma solidity ^0.8.17;
 // Specifies the version of the Solidity compiler to use. In this case, version 0.8.17 or higher is required.
 
+/// @title ValueTypes
+/// @notice A Solidity smart contract showcasing various value types.
 contract ValueTypes {
-    // Declaring a Solidity smart contract named "ValueTypes."
-
     bool public b;
     // Declaring a public boolean variable named "b." Booleans can be either true or false.
 
@@ -25,5 +25,33 @@ contract ValueTypes {
     bytes32 public b32;
     // Declaring a public fixed-size bytes32 variable named "b32." This can hold 32 bytes of data.
 
-    // This contract showcases various value types in Solidity.
+    /// @notice Get the current boolean value.
+    /// @return The boolean value.
+    function getBool() public view returns (bool) {
+        return b;
+    }
+
+    /// @notice Get the current signed integer value.
+    /// @return The signed integer value.
+    function getInt() public view returns (int) {
+        return i;
+    }
+
+    /// @notice Get the current unsigned integer value.
+    /// @return The unsigned integer value.
+    function getUint() public view returns (uint) {
+        return u;
+    }
+
+    /// @notice Get the current Ethereum address.
+    /// @return The Ethereum address.
+    function getAddress() public view returns (address) {
+        return addr;
+    }
+
+    /// @notice Get the current bytes32 value.
+    /// @return The bytes32 value.
+    function getBytes32() public view returns (bytes32) {
+        return b32;
+    }
 }
