e449e8fe8e20aa62cfa171c9d586b6e3015d616e
Rishabh Chauhan <119417999+a-fai1ur3@users.noreply.github.com>
2023-09-12 03:28:18 +0530

    Update forAndwhile.sol

--- forAndwhile.sol
+++ forAndwhile.sol
@@ -1,47 +1,44 @@
 // SPDX-License-Identifier: MIT
-// The above comment specifies the license under which this contract is distributed.
-
 pragma solidity ^0.8.17;
-// Specifies the version of the Solidity compiler to use. In this case, version 0.8.17 or higher is required.
 
+/**
+ * @title ForAndWhileLoops
+ * @notice A Solidity smart contract showcasing for and while loops.
+ */
 contract ForAndWhileLoops {
-    // Declaring a Solidity smart contract named "ForAndWhileLoops."
-
+    /**
+     * @dev Function demonstrating for and while loops.
+     */
     function loop() public pure {
-        // Declaring a public pure function named "loop" with no arguments.
-        
         for (uint i = 0; i < 10; i++) {
-            // Using a for loop to iterate from 0 to 9 (inclusive).
-            
             if (i == 3) {
-                // If the loop variable "i" is equal to 3, continue to the next iteration.
                 continue;
             }
             
             if (i == 5) {
-                // If the loop variable "i" is equal to 5, exit the loop.
                 break;
             }
         }
         
-        uint j; // Declaring an unsigned integer variable "j" for the while loop.
+        uint j;
         
         while (j < 10) {
-            // Using a while loop to increment "j" until it becomes greater than or equal to 10.
             j++;
         }
     }
 
+    /**
+     * @dev Function to calculate the sum of numbers from 1 to n.
+     * @param _n The input unsigned integer.
+     * @return The sum of numbers from 1 to _n.
+     */
     function sum(uint _n) public pure returns (uint) {
-        // Declaring a public pure function named "sum" that takes an unsigned integer argument "_n" and returns an unsigned integer.
-
-        uint add; // Declaring a local unsigned integer variable "add" to store the sum.
+        uint add;
         
         for (uint i = 1; i <= _n; ++i) {
-            // Using a for loop to calculate the sum of numbers from 1 to "_n".
-            add += i; // Adding the current value of "i" to the "add" variable.
+            add += i;
         }
         
-        return add; // Returning the computed sum.
+        return add;
     }
 }
