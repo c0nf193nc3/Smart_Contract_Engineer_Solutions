33ed68d5854a3d1d8a60bf5c6600eefd060587ee
Rishabh Chauhan <119417999+a-fai1ur3@users.noreply.github.com>
2023-09-12 03:23:34 +0530

    Update constants.sol

--- constants.sol
+++ constants.sol
@@ -1,21 +1,18 @@
 // SPDX-License-Identifier: MIT
-// The above comment specifies the license under which this contract is distributed.
-
 pragma solidity ^0.8.17;
-// Specifies the version of the Solidity compiler to use. In this case, version 0.8.17 or higher is required.
 
+/**
+ * @title Constants
+ * @notice A Solidity smart contract showcasing constant state variables.
+ */
 contract Constants {
-    // Declaring a Solidity smart contract named "Constants."
+    /**
+     * @dev Public constant Ethereum address.
+     */
+    address public constant MY_ADDR = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
 
-    address public constant MY_ADDR =
-        0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
-    // Declaring a public constant Ethereum address variable named "MY_ADDR."
-    // The constant value is the specified Ethereum address.
-
+    /**
+     * @dev Public constant unsigned integer.
+     */
     uint public constant MY_UINT = 123;
-    // Declaring a public constant unsigned integer variable named "MY_UINT."
-    // The constant value is 123.
-
-    // Constants in Solidity are variables with values that cannot be changed after deployment.
-    // They are useful for storing values that should remain fixed throughout the contract's lifetime.
 }
