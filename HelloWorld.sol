cd2dced5edce7953ddb7be8fb0167b4819b76899
Rishabh Chauhan <119417999+a-fai1ur3@users.noreply.github.com>
2023-09-12 03:09:48 +0530

    Update HelloWorld.sol

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -1,18 +1,22 @@
 // SPDX-License-Identifier: MIT
-// The above comment specifies the license under which this contract is distributed.
 
 pragma solidity ^0.8.17;
-// Specifies the version of the Solidity compiler to use. In this case, version 0.8.17 or higher is required.
 
-/// @title HelloWorld
-/// @notice A simple Solidity smart contract that stores a greeting message.
+/**
+ * @title HelloWorld
+ * @notice A simple Solidity smart contract that stores a greeting message.
+ */
 contract HelloWorld {
+    /**
+     * @dev Stores the greeting message.
+     * @return The greeting message.
+     */
     string public welcome = "Hello World";
-    // Declaring a public string variable named "welcome" and initializing it with the value "Hello World."
-    // The "public" keyword allows anyone to read the value of this variable.
-    
-    /// @notice Get the current greeting message.
-    /// @return The greeting message.
+
+    /**
+     * @dev Get the current greeting message.
+     * @return The greeting message.
+     */
     function getGreeting() public view returns (string memory) {
         return welcome;
     }
