05178f9da93e75cd66be07cc246cb6700d0921f5
Rishabh Chauhan <119417999+a-fai1ur3@users.noreply.github.com>
2023-09-12 03:44:25 +0530

    Update functionModifier.sol

--- functionModifier.sol
+++ functionModifier.sol
@@ -1,89 +1,91 @@
 // SPDX-License-Identifier: MIT
-// The above comment specifies the license under which this contract is distributed.
-
 pragma solidity ^0.8.17;
-// Specifies the version of the Solidity compiler to use. In this case, version 0.8.17 or higher is required.
 
+/**
+ * @title FunctionModifier
+ * @notice A Solidity smart contract showcasing function modifiers for state management.
+ */
 contract FunctionModifier {
-    // Declaring a Solidity smart contract named "FunctionModifier."
-
     bool public paused; // A public state variable to track whether the contract is paused.
     uint public count; // A public state variable to store a count value.
 
+    /**
+     * @dev Custom modifier to check if the contract is not paused.
+     * @notice Reverts the transaction if the contract is paused.
+     */
     modifier whenNotPaused() {
-        // Declaring a custom modifier named "whenNotPaused."
-        // Modifiers are used to add custom conditions to functions.
-
         require(!paused, "paused");
-        // Using "require" to check if the contract is not paused. If it's paused, revert the transaction with an error message.
-
-        _; // The underscore represents the location where the actual function code will be executed.
-        // In this case, the function code is placed between this modifier and "_;".
+        _; // Function code will be executed here.
     }
-    
+
+    /**
+     * @dev Custom modifier to check if the contract is paused.
+     * @notice Reverts the transaction if the contract is not paused.
+     */
     modifier whenPaused() {
-        // Declaring a custom modifier named "whenPaused."
-
         require(paused, "not paused");
-        // Using "require" to check if the contract is paused. If it's not paused, revert the transaction with an error message.
-
-        _;
+        _; // Function code will be executed here.
     }
 
+    /**
+     * @dev Public function to update the "paused" state variable.
+     * @param _paused The new pause status.
+     * @notice Can be called to pause or unpause the contract.
+     */
     function setPause(bool _paused) public {
-        // A public function named "setPause" to update the "paused" state variable.
-
         paused = _paused;
-        // Updates the "paused" state variable based on the provided input.
     }
 
-    // This function will throw an error if paused
+    /**
+     * @dev Public function to increment the "count" state variable.
+     * @notice Can only be called when the contract is not paused.
+     */
     function inc() public whenNotPaused {
-        // A public function named "inc" that increments the "count" state variable.
-
         count += 1;
-        // This function uses the "whenNotPaused" modifier to ensure that it can only be executed when the contract is not paused.
     }
 
+    /**
+     * @dev Public function to decrement the "count" state variable.
+     * @notice Can only be called when the contract is not paused.
+     */
     function dec() public whenNotPaused {
-        // A public function named "dec" that decrements the "count" state variable.
-
         count -= 1;
-        // This function also uses the "whenNotPaused" modifier to ensure it's not executed when the contract is paused.
     }
 
+    /**
+     * @dev Custom modifier to check if the input value is less than 10.
+     * @param _x The input value to check.
+     * @notice Reverts the transaction if _x is greater than or equal to 10.
+     */
     modifier cap(uint _x) {
-        // Declaring a custom modifier named "cap."
-        // Modifiers can also take input and check it.
-
         require(_x < 10, "x >= 10");
-        // Using "require" to check if the input value "_x" is less than 10. If it's not, revert the transaction with an error message.
-
-        _;
+        _; // Function code will be executed here.
     }
 
+    /**
+     * @dev Public function to increment the "count" state variable by a specified value.
+     * @param _x The value to increment by.
+     * @notice Can only be called when the contract is not paused and _x is less than 10.
+     */
     function incBy(uint _x) public whenNotPaused cap(_x) {
-        // A public function named "incBy" that increments the "count" state variable by a specified value.
-
         count += _x;
-        // This function uses both the "whenNotPaused" and "cap" modifiers to ensure it's not executed when the contract is paused
-        // and that the input value "_x" is less than 10.
     }
-    
+
+    /**
+     * @dev Custom modifier example (sandwich).
+     * @notice This modifier allows executing code before and after the function.
+     */
     modifier sandwich() {
-        // Declaring a custom modifier named "sandwich."
-        // Modifiers can execute code before and after the function or depend on many requirements.
-
-        // code before the function
-        _; // This represents the location where the actual function code will be executed.
-        // In this case, the function code is placed between this modifier and "_;".
-        // code after the function
+        // Code executed before the function.
+        _; // Function code will be executed here.
+        // Code executed after the function.
     }
-    
-    function reset() public whenPaused {
-        // A public function named "reset" that sets the "count" state variable to 0.
-        // It can only be executed when the contract is paused.
 
+    /**
+     * @dev Public function to reset the "count" state variable to 0.
+     * @notice Can only be called when the contract is paused.
+     */
+    function reset() public whenPaused {
         count = 0;
     }
 }
