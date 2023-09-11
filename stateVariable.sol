e0be5d0c76b9737efdfa51cf024f39d3d57683e3
Rishabh Chauhan <119417999+a-fai1ur3@users.noreply.github.com>
2023-09-12 03:14:03 +0530

    Update stateVariable.sol

--- stateVariable.sol
+++ stateVariable.sol
@@ -1,41 +1,44 @@
 // SPDX-License-Identifier: MIT
-// The above comment specifies the license under which this contract is distributed.
-
 pragma solidity ^0.8.17;
-// Specifies the version of the Solidity compiler to use. In this case, version 0.8.17 or higher is required.
 
+/**
+ * @title StateVariables
+ * @notice A Solidity smart contract demonstrating state variables and functions.
+ */
 contract StateVariables {
-    // Declaring a Solidity smart contract named "StateVariables."
-
+    /**
+     * @dev Public unsigned integer state variable.
+     */
     uint public num;
-    // Declaring a public unsigned integer state variable named "num."
-    // This variable can be read by anyone and modified by the contract's functions.
 
+    /**
+     * @dev Set the value of the state variable.
+     * @param _num The new value to set.
+     */
     function setNum(uint _num) external {
-        // Declaring a public external function named "setNum" that takes an unsigned integer argument "_num."
-        // The "external" keyword indicates that this function can be called from outside the contract.
-
-        num = _num; // Set the value of the "num" state variable to the provided "_num" value.
+        num = _num;
     }
 
-    // View - "view" is a read-only function.
-
+    /**
+     * @dev Get the current value of the state variable.
+     * @return The current value of 'num'.
+     */
     function getNum() public view returns (uint) {
-        // Declaring a public view function named "getNum."
-        // The "public" keyword allows anyone to call this function, and "view" indicates that it doesn't modify the contract's state.
-
-        return num; // Return the current value of the "num" state variable.
+        return num;
     }
-    
+
+    /**
+     * @dev Reset the state variable to zero.
+     */
     function resetNum() public {
-        // Declaring a public function named "resetNum."
-
-        num = 0; // Reset the value of the "num" state variable to 0.
+        num = 0;
     }
-    
-    function getNumPlusOne() public view returns (uint) {
-        // Declaring a public view function named "getNumPlusOne."
 
-        return num + 1; // Return the current value of "num" incremented by 1.
+    /**
+     * @dev Get the current value of 'num' incremented by one.
+     * @return The value of 'num' plus one.
+     */
+    function getNumPlusOne() public view returns (uint) {
+        return num + 1;
     }
 }
