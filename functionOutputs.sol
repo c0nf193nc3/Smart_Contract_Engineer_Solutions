27aea9f616d64a619d9e0d44c23e0ffefb41a540
Rishabh Chauhan <119417999+a-fai1ur3@users.noreply.github.com>
2023-09-12 04:41:23 +0530

    Update functionOutputs.sol

--- functionOutputs.sol
+++ functionOutputs.sol
@@ -1,50 +1,57 @@
 // SPDX-License-Identifier: MIT
-// The above comment specifies the license under which this contract is distributed.
-
 pragma solidity ^0.8.17;
-// Specifies the version of the Solidity compiler to use. In this case, version 0.8.17 or higher is required.
 
+/**
+ * @title FunctionOutputs
+ * @notice A Solidity smart contract showcasing various ways of returning multiple values from functions.
+ */
 contract FunctionOutputs {
-    // Declaring a Solidity smart contract named "FunctionOutputs."
-
+    /**
+     * @dev Function to return multiple values as a tuple.
+     * @notice This function returns a tuple containing a uint and a bool.
+     * @return The tuple with values (1, true).
+     */
     function returnMany() public pure returns (uint, bool) {
-        // A public pure function named "returnMany" that returns multiple values (a uint and a bool).
-
         return (1, true);
-        // This function returns a tuple containing two values: 1 (uint) and true (bool).
     }
 
+    /**
+     * @dev Function to return multiple values with named outputs.
+     * @notice This function returns a tuple with named outputs "x" and "b."
+     * @return The tuple with values (x: 1, b: true).
+     */
     function named() public pure returns (uint x, bool b) {
-        // A public pure function named "named" that returns multiple values with named outputs.
-
         return (1, true);
-        // This function returns a tuple containing two values: 1 (assigned to "x") and true (assigned to "b").
     }
 
+    /**
+     * @dev Function to return multiple values with named outputs and assignments.
+     * @notice This function assigns values to named outputs "x" and "b" before returning them.
+     * @return The tuple with values (x: 1, b: true).
+     */
     function assigned() public pure returns (uint x, bool b) {
-        // A public pure function named "assigned" that returns multiple values with named outputs.
-
         x = 1;
         b = true;
-        // This function assigns values to the named outputs "x" and "b" before returning them.
     }
 
+    /**
+     * @dev Function demonstrating destructuring assignment of function outputs.
+     * @notice This function calls "returnMany" and assigns its return values to "i" and "b."
+     * @notice It also showcases how to ignore left-out values in the tuple during destructuring.
+     */
     function destructuringAssigments() public pure {
-        // A public pure function named "destructuringAssigments" that demonstrates destructuring assignment of function outputs.
-
         (uint i, bool b) = returnMany();
-        // This line calls the "returnMany" function and assigns its return values to "i" and "b."
-        // The values are unused in this example, but it showcases how to destructure the return values.
 
-        // Left out values in the tuple can be ignored during destructuring.
+        // Ignoring the first value (uint) from "returnMany" and capturing only the second (bool).
         (, bool a) = returnMany();
-        // Here, we only capture the second value (bool) from the "returnMany" function and ignore the first (uint).
     }
     
+    /**
+     * @dev Function to return an address and a bool.
+     * @notice This function returns a tuple containing the sender's address (msg.sender) and false (bool).
+     * @return The tuple with values (address: msg.sender, bool: false).
+     */
     function myFunc() public view returns (address, bool) {
-        // A public view function named "myFunc" that returns an address and a bool.
-
         return (msg.sender, false);
-        // This function returns a tuple containing two values: the sender's address (msg.sender) and false (bool).
     }
 }
