fbdcea46432454de8072fe40a8e93a70ffdd0aa5
Rishabh Chauhan <119417999+a-fai1ur3@users.noreply.github.com>
2023-09-12 03:15:31 +0530

    Update localVariables.sol

--- localVariables.sol
+++ localVariables.sol
@@ -1,26 +1,27 @@
 // SPDX-License-Identifier: MIT
-// The above comment specifies the license under which this contract is distributed.
-
 pragma solidity ^0.8.17;
-// Specifies the version of the Solidity compiler to use. In this case, version 0.8.17 or higher is required.
 
+/**
+ * @title LocalVariables
+ * @notice A Solidity smart contract demonstrating local variables and functions.
+ */
 contract LocalVariables {
-    // Declaring a Solidity smart contract named "LocalVariables."
-
+    /**
+     * @dev Function to showcase local variables.
+     * @return An uninitialized unsigned integer 'u' and an uninitialized boolean 'b'.
+     */
     function localVars() public pure returns (uint, bool) {
-        // Declaring a public function named "localVars" with no arguments.
-        // This function is marked as "pure," meaning it doesn't modify the contract's state.
-
         uint u; // Declaring a local unsigned integer variable "u" without initializing it.
         bool b; // Declaring a local boolean variable "b" without initializing it.
 
         return (u, b); // Returning the uninitialized local variables "u" and "b."
     }
     
+    /**
+     * @dev Function to multiply a constant.
+     * @return The result of multiplying 123456 by itself.
+     */
     function mul() public pure returns (uint) {
-        // Declaring a public function named "mul" with no arguments.
-        // This function is marked as "pure," meaning it doesn't modify the contract's state.
-
         uint x = 123456; // Declaring a local unsigned integer variable "x" and initializing it with the value 123456.
 
         return x * x; // Returning the result of multiplying "x" by itself, which is 123456 * 123456.
