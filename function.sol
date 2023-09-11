c6752c2dbb5720fbf4d5a4654af9e5511c4338b7
Rishabh Chauhan <119417999+a-fai1ur3@users.noreply.github.com>
2023-09-12 03:11:58 +0530

    Update function.sol

--- function.sol
+++ function.sol
@@ -1,25 +1,28 @@
 // SPDX-License-Identifier: MIT
-// The above comment specifies the license under which this contract is distributed.
-
 pragma solidity ^0.8.17;
-// Specifies the version of the Solidity compiler to use. In this case, version 0.8.17 or higher is required.
 
+/**
+ * @title FunctionIntro
+ * @notice A Solidity smart contract showcasing basic arithmetic functions.
+ */
 contract FunctionIntro {
-    // Declaring a Solidity smart contract named "FunctionIntro."
-
+    /**
+     * @dev Adds two unsigned integers.
+     * @param x The first unsigned integer.
+     * @param y The second unsigned integer.
+     * @return The result of the addition.
+     */
     function add(uint x, uint y) public pure returns (uint) {
-        // Declaring a public function named "add" that takes two unsigned integer arguments, "x" and "y."
-        // The "public" keyword allows anyone to call this function.
-        // The "pure" keyword indicates that this function doesn't modify the contract's state.
-
-        return x + y; // Perform addition of "x" and "y" and return the result.
+        return x + y;
     }
     
+    /**
+     * @dev Subtracts one unsigned integer from another.
+     * @param x The first unsigned integer.
+     * @param y The second unsigned integer to subtract from the first.
+     * @return The result of the subtraction.
+     */
     function sub(uint x, uint y) public pure returns (uint) {
-        // Declaring a public function named "sub" that takes two unsigned integer arguments, "x" and "y."
-        // The "public" keyword allows anyone to call this function.
-        // The "pure" keyword indicates that this function doesn't modify the contract's state.
-
-        return x - y; // Perform subtraction of "y" from "x" and return the result.
+        return x - y;
     }
 }
