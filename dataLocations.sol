8983230905afda2cf1db722f6a12161e2a0fa87e
Rishabh Chauhan <119417999+a-fai1ur3@users.noreply.github.com>
2023-09-12 11:53:00 +0530

    Update dataLocations.sol

--- dataLocations.sol
+++ dataLocations.sol
@@ -1,69 +1,52 @@
 // SPDX-License-Identifier: MIT
-// The above comment specifies the license under which this contract is distributed.
 
 pragma solidity ^0.8.17;
-// Specifies the version of the Solidity compiler to use. In this case, version 0.8.17 or higher is required.
 
+/**
+ * @title DataLocations
+ * @dev This contract demonstrates data locations in Solidity.
+ */
 contract DataLocations {
-    // Declaring a Solidity smart contract named "DataLocations."
-
     uint public x;
-    // Declaring a public state variable "x" of type uint. State variables have a data location of "storage" by default.
-
     uint public arr;
-    // Declaring another public state variable "arr" of type uint. Also stored in "storage" by default.
 
     struct MyStruct {
         uint foo;
         string text;
     }
-    // Defining a struct named "MyStruct" with two fields: "foo" of type uint and "text" of type string.
 
     mapping(address => MyStruct) public myStructs;
-    // Declaring a public mapping named "myStructs" that associates Ethereum addresses with "MyStruct" values.
-    // Mappings store data in "storage" by default.
 
+    /**
+     * @dev Demonstrates data locations.
+     * @return memArr A dynamic memory array.
+     */
     function examples() public returns (uint[] memory) {
-        // A public function named "examples" that demonstrates data locations.
-
         myStructs[msg.sender] = MyStruct({foo: 123, text: "bar"});
-        // Initializing the "myStructs" mapping with a "MyStruct" value for the sender's Ethereum address.
-        // This operation stores data in "storage."
-
         MyStruct storage myStruct = myStructs[msg.sender];
-        // Creating a reference "myStruct" to the "MyStruct" value associated with the sender's Ethereum address.
-        // This is an example of a "storage" reference.
-
         myStruct.text = "baz";
-        // Modifying the "text" field of the referenced "myStruct."
-        // Since "myStruct" is a storage reference, this operation modifies data in "storage."
 
         uint[] memory memArr = new uint[](3);
-        // Creating a dynamic memory array "memArr" with a length of 3.
-        // This array is stored in "memory."
-
         memArr[1] = 123;
-        // Modifying an element of the "memArr" array.
-        // This operation works with data in "memory."
 
         return memArr;
-        // Returning the "memArr" array, which is read from "memory."
     }
 
+    /**
+     * @dev Allows setting the "text" field for a specified address.
+     * @param _addr The Ethereum address for which to set the "text" field.
+     * @param _text The new text value.
+     */
     function set(address _addr, string calldata _text) public {
-        // A public function named "set" that allows setting the "text" field for a specified address.
-        // Notice that "_text" is declared as "calldata" instead of "memory" to minimize gas cost.
-
         myStructs[_addr].text = _text;
-        // Modifying the "text" field of the "MyStruct" associated with the provided address "_addr."
-        // This operation interacts with data in "storage."
     }
 
-    function get(address _addr) public view returns (string memory) {
-        // A public view function named "get" that retrieves the "text" field for a specified address.
-
+    /**
+     * @dev Retrieves the "text" field for a specified address.
+     * @param _addr The Ethereum address for which to retrieve the "text" field.
+     * @return text The text value associated with the provided address.
+     */
+    function get(address _addr) public view returns (string memory text) {
         return myStructs[_addr].text;
-        // Returning the "text" field of the "MyStruct" associated with the provided address "_addr."
-        // This operation reads data from "storage."
     }
 }
