d7b363fc687045188732c7e9b3cc5e95b94f7cae
Rishabh Chauhan <119417999+a-fai1ur3@users.noreply.github.com>
2023-09-12 20:44:51 +0530

    Update fallback.sol

--- fallback.sol
+++ fallback.sol
@@ -1,26 +1,29 @@
 // SPDX-License-Identifier: MIT
-// The above comment specifies the license under which this contract is distributed.
 
 pragma solidity ^0.8.17;
-// Specifies the version of the Solidity compiler to use. In this case, version 0.8.17 or higher is required.
 
+/**
+ * @title Fallback
+ * @dev This contract represents a contract with a fallback and receive function to handle Ether transfers.
+ */
 contract Fallback {
-    // Declaring a Solidity smart contract named "Fallback."
-
     string[] public answers = ["receive", "fallback"];
-    // Declaring a public state variable "answers" as an array of strings to store two answers.
 
+    /**
+     * @dev Fallback function.
+     * Executed when the contract receives Ether without a specific function call or when a function call fails.
+     * Used for custom behavior when funds are sent to the contract.
+     */
     fallback() external payable {
-        // The fallback function, declared as external and payable.
-        // This function is executed when the contract receives Ether without a specific function call or when a function call fails.
-
-        // It is common to use the fallback function to handle unexpected Ether transfers or to provide custom behavior when funds are sent to the contract.
+        // Custom behavior for handling unexpected Ether transfers or failed function calls.
     }
 
+    /**
+     * @dev Receive function.
+     * Executed when the contract receives Ether with a simple transfer, e.g., "address.transfer(value)".
+     * A newer and more efficient way to handle Ether transfers compared to the fallback function.
+     */
     receive() external payable {
-        // The receive function, declared as external and payable.
-        // This function is executed when the contract receives Ether with a simple transfer, such as "address.transfer(value)".
-
-        // It is a newer and more efficient way to handle Ether transfers compared to the fallback function.
+        // Custom behavior for handling Ether transfers using "receive" method.
     }
 }
