f901fc1bea4bfa77a18edba5daf9aaa20c68e6d6
Rishabh Chauhan <119417999+a-fai1ur3@users.noreply.github.com>
2023-09-12 11:53:51 +0530

    Update simpleStorage.sol

--- simpleStorage.sol
+++ simpleStorage.sol
@@ -1,30 +1,27 @@
 // SPDX-License-Identifier: MIT
-// The above comment specifies the license under which this contract is distributed.
 
 pragma solidity ^0.8.17;
-// Specifies the version of the Solidity compiler to use. In this case, version 0.8.17 or higher is required.
 
+/**
+ * @title SimpleStorage
+ * @dev This contract demonstrates a simple storage contract in Solidity.
+ */
 contract SimpleStorage {
-    // Declaring a Solidity smart contract named "SimpleStorage."
-
     string public text;
-    // Declaring a public state variable "text" of type string. State variables are stored in "storage."
 
+    /**
+     * @dev Allows setting the value of the "text" variable.
+     * @param _text The new value to set.
+     */
     function set(string calldata _text) public {
-        // A public function named "set" that allows setting the value of the "text" variable.
-        // The function takes a string argument "_text" declared as "calldata."
-
         text = _text;
-        // Assigning the value of "_text" to the "text" state variable.
-        // This operation stores the new value in "storage."
     }
 
+    /**
+     * @dev Allows retrieving the current value of the "text" variable.
+     * @return The current value of the "text" variable.
+     */
     function get() public view returns (string memory) {
-        // A public view function named "get" that allows retrieving the current value of the "text" variable.
-        // It returns a string stored in "memory."
-
         return text;
-        // Returning the current value of the "text" state variable.
-        // This operation reads the value from "storage" and returns it to the caller.
     }
 }
