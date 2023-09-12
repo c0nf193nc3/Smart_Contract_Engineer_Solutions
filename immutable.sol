555aa67994f3b5f9a4e213418e1846baae434082
Rishabh Chauhan <119417999+a-fai1ur3@users.noreply.github.com>
2023-09-12 20:42:17 +0530

    Update immutable.sol

--- immutable.sol
+++ immutable.sol
@@ -1,21 +1,21 @@
 // SPDX-License-Identifier: MIT
-// The above comment specifies the license under which this contract is distributed.
 
 pragma solidity ^0.8.17;
-// Specifies the version of the Solidity compiler to use. In this case, version 0.8.17 or higher is required.
 
+/**
+ * @title Immutable
+ * @dev This contract represents an immutable contract with an 'owner' state variable.
+ * The 'owner' variable can only be set once during contract deployment.
+ */
 contract Immutable {
-    // Declaring a Solidity smart contract named "Immutable."
-
     address public immutable owner;
-    // Declaring a public state variable named "owner" with the `immutable` keyword.
-    // The `immutable` keyword indicates that this variable's value can only be set once during contract deployment.
 
+    /**
+     * @dev Constructor for the Immutable contract.
+     * Sets the 'owner' state variable to the address of the contract deployer.
+     */
     constructor() {
-        // A constructor function that is executed once during contract deployment.
-
         owner = msg.sender;
-        // Assigning the address of the sender (creator of the contract) to the "owner" state variable.
-        // Since "owner" is declared as `immutable`, its value can only be set once during contract creation.
+        // The 'immutable' keyword ensures that 'owner' can only be set once during contract creation.
     }
 }
